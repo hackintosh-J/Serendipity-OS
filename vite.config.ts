81c1a6accbf3f1cccfefb76112546a56a7c77980
hackintosh-J <cxhcxh2233@gmail.com>
2025-08-30 20:55:36 +0800

    github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
+// vite.config.ts
+
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // 在这里添加 base 属性
+      base: './', // 或者 '/your-repo-name/'
+
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
